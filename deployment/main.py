04aa898dcb8efa174fe9a821a67209e2d15c1762
Thanapat Teerarattanyu <37036113+TJTHANAPAT@users.noreply.github.com>
2022-05-03 00:22:37 +0700

    Update main.py

--- deployment/main.py
+++ deployment/main.py
@@ -1,8 +1,11 @@
 from tkinter import *
+from tkinter import filedialog
 import tkinter as tk
 import win32gui
 from PIL import ImageGrab
+import cv2
 import numpy as np
+
 
 from model import predict_digit
 from digit_segmentation import segment_digits
@@ -16,22 +19,36 @@ class App(tk.Tk):
         # Creating elements
         self.canvas = tk.Canvas(
             self, width=500, height=300, bg="white", cursor="cross")
-        self.label = tk.Label(self, text="Write Number",
-                              font=("Tahoma", 32))
+        self.label = tk.Label(self, text="Write Number or Upload File",
+                              font=("Tahoma", 28))
         self.classify_btn = tk.Button(
             self, text="Recognise", command=self.classify_handwriting)
-        self.button_clear = tk.Button(
+        self.clear_btn = tk.Button(
             self, text="Clear", command=self.clear_all)
+        self.upload_btn = tk.Button(
+            self, text='Upload File', command=self.upload_file)
         # Grid structure
         self.canvas.grid(row=0, column=0, columnspan=2, pady=2, sticky=W, )
         self.label.grid(row=1, column=0, columnspan=2, pady=2, padx=2)
         self.classify_btn.grid(row=2, column=1, pady=20)
-        self.button_clear.grid(row=2, column=0, pady=20)
+        self.clear_btn.grid(row=2, column=0, pady=20)
+        self.upload_btn.grid(row=3, column=0, columnspan=2, pady=20)
         self.canvas.bind("<B1-Motion>", self.draw_lines)
+
+    def upload_file(self):
+        f_types = [('JPEG Files', '*.jpg'), ('PNG Files', '*.png')]
+        filename = filedialog.askopenfile(filetypes=f_types).name
+        img = cv2.imread(filename)
+        digits = segment_digits(img)
+        result = ''
+        for digit in digits:
+            digit = predict_digit(digit)
+            result = result + str(digit)
+        self.label.configure(text=result)
 
     def clear_all(self):
         self.canvas.delete("all")
-        self.label.configure(text="Write Number")
+        self.label.configure(text="Write Number or Upload File")
 
     def classify_handwriting(self):
         self.label.configure(text='Recognising...')
